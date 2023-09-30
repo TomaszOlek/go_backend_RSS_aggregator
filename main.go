b48bcd8b5f2c14387c4007e02f92a0d958655dc3
TomaszOlek <tomasz.olek.dev@gmail.com>
2023-09-30 14:02:39 +0200

    Authentication w/ API keys

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 	v1Router.Get("/healthz", handleReadiness)
 	v1Router.Get("/err", handleErr)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.handlerGetUser)
 
 	router.Mount("/v1", v1Router)
 
